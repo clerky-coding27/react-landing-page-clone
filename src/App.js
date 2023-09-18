f9a25328e4fdd38b4e55864a83e89fce78ccd3fb
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-18 14:26:03 +0200

    Create skeleton for nav and hero and import google fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import './App.css';
-import logo from './logo.svg';
+import './my-sass.scss';
 
 export default function App() {
   return (
-    <div className="App">
+    <body className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href="/#">Brevo Logo SVG PH</a>
+        <nav>
+          <ul>
+            <li>Products</li>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Resources</li>
+          </ul>
+        </nav>
+        <a href="/#">Globe PH</a>
+        <ul>
+          <li>Login</li>
+          <li>Sign Up Free</li>
+        </ul>
       </header>
-    </div>
+      <main>
+        <h1>Grow revenue with the most approachable CRM</h1>
+        <p>
+          The all-in-one platform to automate your marketing campaigns over
+          Email, SMS, WhatsApp or chat. Join the 500,000 companies who trust
+          Brevo.
+        </p>
+        <a href="/#">Sign Up Free</a>
+      </main>
+    </body>
   );
 }
